8d60143dd7ce6ebf2706f04d2dbe32e82841c361
Slipp Douglas Thompson <git@slippyd.com>
2014-06-18 23:39:06 -0400

    Initial implementation of texture generator
    In ViewController:
    
    • New `destImage` getter will return the `_destImage` if set, otherwise creates the image via a CGImage and CGDataProvider that trigger the image generation.
    	• New logic in `viewDidLoad` and `pointsDidChange` to trigger image generation/regeneration.
    		» Instead of the temporary patch of setting the `_destImage` to equal the `_srcImage`!
    • `genDestImageBytesAtPosition` is a CGDataProviderGetBytesAtPositionCallback-conforming function that — for now — simply sets all pixels to transparent black.
    	» Trying to figure out how to best use CGDataProviderCreateDirect() API.  It seems to just ask for all the data at once which seems kinda pointless.  The docs say that this approach is good for JIT delivery of image data.

--- DrawInQuad/ViewController.m
+++ DrawInQuad/ViewController.m
@@ -6,6 +6,10 @@
 @interface ViewController () {
 	UIImage *_srcImage;
 	UIImage *_destImage;
+	
+	size_t _destImageTotalBytes;
+	unsigned int _destImageWidth;
+	unsigned int _destImageHeight;
 }
 
 - (CGPoint)handleCenterFromPoint:(CGPoint)point;
@@ -46,6 +50,91 @@
 	self.outlineView[3] = [NSValue valueWithCGPoint:point];
 }
 
+size_t genDestImageBytesAtPosition(void *info, void *buffer, off_t position, size_t requestedByteCount)
+{
+	ViewController *self = (__bridge ViewController *)info;
+	unsigned int width = self->_destImageWidth,
+		height = self->_destImageHeight,
+		pixelCount = width * height;
+	
+	char *byteBuffer = (char *)buffer;
+	
+	unsigned int pixelIndex = (unsigned int)(position / 4);
+	if (pixelIndex >= pixelCount)
+		return 0;
+	unsigned int pixelX = pixelIndex % width,
+		pixelY = pixelIndex / width;
+	CGFloat pixelU = (CGFloat)pixelX / width,
+		pixelV = (CGFloat)pixelY / height;
+	
+	unsigned int pixelByteOffset = position % 4;
+	size_t byteCount = 4 - pixelByteOffset;
+	
+	switch (pixelByteOffset)
+	{
+		case 0:
+			byteBuffer[0] = 0;
+		case 1:
+			byteBuffer[1] = 0;
+		case 2:
+			byteBuffer[2] = 0;
+		case 3:
+			byteBuffer[3] = 0;
+			break;
+		
+		default:
+			assert(pixelByteOffset < 4);
+	}
+	
+	return byteCount;
+}
+
+- (UIImage *)destImage
+{
+	if (!_destImage) {
+		CGSize srcImageSize = _srcImage.size;
+		_destImageWidth = srcImageSize.width;
+		_destImageHeight = srcImageSize.height;
+		
+		CGImageRef srcCGImage = _srcImage.CGImage;
+		
+		size_t bitsPerComponent = CGImageGetBitsPerComponent(srcCGImage);
+		size_t bitsPerPixel = bitsPerComponent * 4;
+		size_t bitsPerRow = bitsPerPixel * _destImageWidth,
+			bytesPerRow = bitsPerRow >> 3;
+		_destImageTotalBytes = bytesPerRow * _destImageHeight;
+		
+		CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+		CGDataProviderRef dataProvider = CGDataProviderCreateDirect(
+			(__bridge void *)self,
+			_destImageTotalBytes,
+			&((CGDataProviderDirectCallbacks){
+				.version = 0,
+				.getBytePointer = NULL,
+				.releaseBytePointer = NULL,
+				.getBytesAtPosition = genDestImageBytesAtPosition,
+				.releaseInfo = NULL
+			})
+		);
+		
+		CGImageRef destCGImage = CGImageCreate(
+			_destImageWidth, _destImageHeight,
+			bitsPerComponent, bitsPerPixel, bytesPerRow,
+			colorSpace,
+			kCGBitmapByteOrderDefault | kCGImageAlphaLast,
+			dataProvider,
+			NULL,
+			FALSE /* shouldInterpolate */,
+			kCGRenderingIntentDefault
+		);
+		_destImage = [[UIImage alloc] initWithCGImage:destCGImage];
+		CGImageRelease(destCGImage);
+		
+		CGDataProviderRelease(dataProvider);
+		CGColorSpaceRelease(colorSpace);
+	}
+	return _destImage;
+}
 
 - (void)viewDidLoad
 {
@@ -64,8 +153,7 @@
 	
 	_srcImage = [UIImage imageNamed:@"test"];
 	
-	_destImage = _srcImage;
-	_imageView.image = _destImage;
+	_imageView.image = self.destImage;
 }
 
 - (void)didReceiveMemoryWarning
@@ -106,6 +194,9 @@
 	self.handle2.center = [self handleCenterFromPoint:self.point2];
 	self.handle3.center = [self handleCenterFromPoint:self.point3];
 	self.handle4.center = [self handleCenterFromPoint:self.point4];
+	
+	_destImage = nil;
+	_imageView.image = self.destImage; // force refresh, for now
 }
 
 - (IBAction)draggedHandle:(id)sender
