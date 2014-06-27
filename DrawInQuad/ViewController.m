9feeee950450f6664c760ba0cd4c347fd442cf02
Slipp Douglas Thompson <git@slippyd.com>
2014-06-27 02:43:39 -0400

    Barycentric UV Coords
    Trying out using Barycentric coordinates for UV calculation.  Works, but there’s a sharp line between each tri.  There might be an alternative Barycentric-like algorithm for quads, or bilinear might be our best bet.  On the plus side, this barycentric calculation doesn’t seem to suffer from the 1st/3rd mirror issue that my bilinear calculation has.
    
    • Added new surfaceSTToTexelUV_barycentricQuad() function— splits the quad into two tris, decides which the point is within, and then delegates to surfaceSTToTexelUV_barycentricTri().
    	• New surfaceSTToTexelUV_barycentricTri() function maps the barycentric coords to UVs.
    	• New barycentricCoords2() function does the computation of UVs.
    		» Founds a few broken implementations of this on the web; ended up using the one from the MK Real-Time Collision Detection book.
    	• New GLKVector2CrossProduct() support function, which simply rearranges the values (and inverts one) and plugs them into GLKVector2DotProduct.
    		» A hand-written NEON-accelerated version would be ideal, unfortunately my attempts have bee not-so-successful.
    	• Another util: GLKVector2Perp(); which does a simple 90°-counter-clock rotation.
    • Switched genDestImagePixelBytesAtPosition() to use surfaceSTToTexelUV_barycentricQuad() (for now).
    • Minor: Added commented-out `@debug` lines to the byte assignment in genDestImagePixelBytesAtPosition() to be able to temporarily visualize the UV values instead of the texture.
    • Regorg: Renamed surfaceSTToTexelUV() to surfaceSTToTexelUV_bilinearQuad(), since we have multiple algorithms implemented for this operation now.

--- DrawInQuad/ViewController.m
+++ DrawInQuad/ViewController.m
@@ -38,6 +38,22 @@ static NSString *kLastWrapUVsNameKey = @"ViewController_WrapUVsName";
 		return GLKVector2LengthSqr(GLKVector2Subtract(vectorEnd, vectorStart));
 	}
 
+	static inline float GLKVector2CrossProduct(GLKVector2 vectorLeft, GLKVector2 vectorRight)
+	{
+		return GLKVector2DotProduct(
+			GLKVector2Make(vectorLeft.x, -vectorLeft.y),
+			GLKVector2Make(vectorRight.y, vectorRight.x)
+		);
+	}
+
+	/// Returns a vector perpendicular to the given vector, rotate 90° counter-clockwise.
+	/// Devised by F. S. Hill Jr. in Graphics Gems IV (1994).
+	///		@source: http://mathworld.wolfram.com/PerpendicularVector.html
+	static inline GLKVector2 GLKVector2Perp(GLKVector2 vector)
+	{
+		return GLKVector2Make(-vector.y, vector.x);
+	}
+
 	static inline int modulo(int dividendA, int divisorN)
 	{
 		if (divisorN < 0) // you can check for divisorN == 0 separately and do what you want
@@ -74,6 +90,25 @@ static NSString *kLastWrapUVsNameKey = @"ViewController_WrapUVsName";
 		}
 		
 		return ratioAlongSegment;
+	}
+
+	static inline GLKVector3 barycentricCoords2(GLKVector2 point, GLKVector2 tri[3])
+	{
+		GLKVector2 v0 = GLKVector2Subtract(tri[1], tri[0]),
+			v1 = GLKVector2Subtract(tri[2], tri[0]),
+			v2 = GLKVector2Subtract(point, tri[0]);
+		
+		float d00 = GLKVector2DotProduct(v0, v0),
+			d01 = GLKVector2DotProduct(v0, v1),
+			d11 = GLKVector2DotProduct(v1, v1),
+			d20 = GLKVector2DotProduct(v2, v0),
+			d21 = GLKVector2DotProduct(v2, v1);
+		
+		float denomReciprocal = 1.0f / (d00 * d11 - d01 * d01);
+		float v = (d11 * d20 - d01 * d21) * denomReciprocal;
+		float w = (d00 * d21 - d01 * d20) * denomReciprocal;
+		
+		return GLKVector3Make(1.0f - v - w, v, w);
 	}
 
 #pragma clang diagnostic pop
@@ -138,7 +173,7 @@ static NSString *kLastWrapUVsNameKey = @"ViewController_WrapUVsName";
 /// Based on a loose understanding of Wikipedia's article on Bilinear interpolation (https://en.wikipedia.org/wiki/Bilinear_interpolation).
 /// 	Probably not the best algoritm for this— works, but with more distortion as the points become less square.
 /// 	Seems to show better results when the left and right sides of the points quad are parallel.
-GLKVector2 surfaceSTToTexelUV(GLKVector2 surfaceST, GLKVector2 pointSTs[4], GLKVector2 pointUVs[4])
+GLKVector2 surfaceSTToTexelUV_bilinearQuad(GLKVector2 surfaceST, GLKVector2 pointSTs[4], GLKVector2 pointUVs[4])
 {
 	GLKVector2 nearestPointOn03Segment, nearestPointOn12Segment;
 	float ratioAlong03 = ratioAlongSegment(surfaceST, pointSTs[0], pointSTs[3], &nearestPointOn03Segment);
@@ -149,6 +184,49 @@ GLKVector2 surfaceSTToTexelUV(GLKVector2 surfaceST, GLKVector2 pointSTs[4], GLKV
 	
 	GLKVector2 texelUV = GLKVector2Make(ratioBetweenNearest03And12, ratiosAlong03And12Rebalanced);
 	return texelUV;
+}
+
+/// @source: Real-Time Collision Detection by Christer Ericson (Morgan Kaufmann, 2005) - Chapter 3: A Math and Geometry Primer - Section 3.4 Barycentric Coordinates
+GLKVector2 surfaceSTToTexelUV_barycentricTri(GLKVector2 surfaceST, GLKVector2 pointSTs[3], GLKVector2 pointUVs[3])
+{
+	GLKVector3 barycentricCoords = barycentricCoords2(surfaceST, pointSTs);
+	
+	GLKVector2 pointUVsPortioned[3] = {
+		GLKVector2MultiplyScalar(pointUVs[0], barycentricCoords.v[0]),
+		GLKVector2MultiplyScalar(pointUVs[1], barycentricCoords.v[1]),
+		GLKVector2MultiplyScalar(pointUVs[2], barycentricCoords.v[2])
+	};
+	
+	GLKVector2 texelUV = GLKVector2Add(GLKVector2Add(pointUVsPortioned[0], pointUVsPortioned[1]), pointUVsPortioned[2]);
+	return texelUV;
+}
+
+GLKVector2 surfaceSTToTexelUV_barycentricQuad(GLKVector2 surfaceST, GLKVector2 pointSTs[4], GLKVector2 pointUVs[4])
+{
+	static const int kStarboardTriInQuadIndices[3] = { 0, 1, 2 };
+	static const int kPortTriInQuadIndices[3] = { 0, 2, 3 };
+	
+	//// @source http://stackoverflow.com/questions/1560492/how-to-tell-whether-a-point-is-to-the-right-or-left-side-of-a-line
+	float lineVsPointCross = GLKVector2CrossProduct(
+		GLKVector2Subtract(pointSTs[2], pointSTs[0]),
+		GLKVector2Subtract(surfaceST, pointSTs[0])
+	);
+	BOOL starboardSide = lineVsPointCross > 0.0f;
+	
+	const int *triInQuadIndices = starboardSide ? kStarboardTriInQuadIndices : kPortTriInQuadIndices;
+	return surfaceSTToTexelUV_barycentricTri(
+		surfaceST,
+		(GLKVector2[3]){
+			pointSTs[triInQuadIndices[0]],
+			pointSTs[triInQuadIndices[1]],
+			pointSTs[triInQuadIndices[2]]
+		},
+		(GLKVector2[3]){
+			pointUVs[triInQuadIndices[0]],
+			pointUVs[triInQuadIndices[1]],
+			pointUVs[triInQuadIndices[2]]
+		}
+	);
 }
 
 static inline GLKVector2 GLKVector2FromCGPoint(CGPoint point) {
@@ -174,7 +252,7 @@ size_t genDestImagePixelBytesAtPosition(void *info, void *buffer, off_t position
 	
 	const CGPoint points[4] = { self.point1, self.point2, self.point3, self.point4 };
 	
-	const GLKVector2 texelUV = surfaceSTToTexelUV(
+	const GLKVector2 texelUV = surfaceSTToTexelUV_barycentricQuad(
 		GLKVector2Make((CGFloat)pixelX / width, (CGFloat)pixelY / height),
 		(GLKVector2[4]){
 			GLKVector2FromCGPoint(points[0]),
@@ -223,12 +301,16 @@ size_t genDestImagePixelBytesAtPosition(void *info, void *buffer, off_t position
 	{
 		case 0:
 			byteBuffer[0] = nearestTexelSample[0];
+			//byteBuffer[0] = fabsf(texelUV.x) * 255.0f; // @debug: show UVs directly
 		case 1:
 			byteBuffer[1] = nearestTexelSample[1];
+			//byteBuffer[1] = fabsf(texelUV.y) * 255.0f; // @debug: show UVs directly
 		case 2:
 			byteBuffer[2] = nearestTexelSample[2];
+			//byteBuffer[2] = (texelUV.x < 0 ? 127 : 0) + (texelUV.y < 0 ? 127 : 0); // @debug: show UVs directly
 		case 3:
 			byteBuffer[3] = nearestTexelSample[3];
+			//byteBuffer[3] = 255; // @debug: show UVs directly
 			break;
 		
 		default:
